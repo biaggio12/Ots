66529041fbe74aced99b5b2f0dcd38993c67a7c1
root <root@vps500550.ovh.net>
2018-01-16 00:17:14 +0100

    Few changes by day

--- data/raids/scripts/omrafir.lua
+++ data/raids/scripts/omrafir.lua
@@ -1,4 +1,7 @@
 function onRaid()
-	local monster = Game.createMonster("Omrafir", Position(33587, 32378, 12)) 
+	local monster = Game.createMonster("Omrafir", Position(33587, 32378, 12))
 	 monster:setReward(true)
 end
+
+
+
